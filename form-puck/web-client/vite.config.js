640893d7f92e2e6d0fbd29db101aec1c85108ea5
Tan Ngo <tannm@upp-technology.com>
2026-06-03 17:27:30 +0200

    fix: set correct base path for GitHub Pages deployment

--- form-puck/web-client/vite.config.js
+++ form-puck/web-client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/demola/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@mediapipe/pose', '@mediapipe/camera_utils', '@mediapipe/drawing_utils']

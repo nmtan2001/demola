2ce64fb038f397ebe651a14cf1a3ba07e63aa69a
Tan Ngo <tannm@upp-technology.com>
2026-06-03 15:37:12 +0200

    add html version

---
+++ web-client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Form Puck</h1>
+      <p>Local pose processing client.</p>
+    </div>
+  )
+}
+
+export default App
